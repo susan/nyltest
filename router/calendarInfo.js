8be41a7d3eff0abf18208ae60fcb2a42ce1d0e1d
susan <susan@users.noreply.github.com>
2019-08-18 11:57:51 -0400

    create calendar event

--- router/calendarInfo.js
+++ router/calendarInfo.js
@@ -7,9 +7,8 @@ router1.get('/',function(req, res, next) {
  });
 
 router1.get('/eventList', function(req, res, next) {
-  console.log("is this working?")
   const nylas = Nylas.with(req.session.token)
-  nylas.events.list({limit: 2 })
+  nylas.events.list({limit: 8 })
   .then(eventList => {
    res.render('CalendarInfoList',
    	{events: eventList,
@@ -17,5 +16,45 @@ router1.get('/eventList', function(req, res, next) {
    	})
   })
 });
+
+router1.post('/event', function(req, res, next) {
+    //console.log(Object.keys(req.body))
+
+    const startDate = `${req.body.eventDate} ${req.body.start}`;
+    const endDate = `${req.body.eventDate} ${req.body.end}`;
+
+    const unixStart = moment(new Date(startDate)).unix();
+    const unixEnd = moment(new Date(endDate)).unix();
+
+    // const eventInfo = {
+	   //  title: req.body.title,
+    //   calendarId: req.body.calendarId,
+    //   when: { start_time: unixStart, end_time: unixEnd },
+    //    participants: [
+    //    { email: req.body.firstInviteeEmail, name: req.body.firstInviteeName },
+	   //  { email: req.body.secondInviteeEmail, name: req.body.secondInviteeName }],
+    //  location: req.body.location,
+    // }
+
+    //console.log(eventInfo)
+   const nylas = Nylas.with(req.session.token)
+   const event = nylas.events.build({
+     title: req.body.title,
+      calendarId: req.body.calendarId,
+      when: { start_time: unixStart, end_time: unixEnd },
+       participants: [
+       { email: req.body.firstInviteeEmail, name: req.body.firstInviteeName },
+	    { email: req.body.secondInviteeEmail, name: req.body.secondInviteeName }
+	    ],
+     location: req.body.location,
+   })
+
+   //res.send("hi")
+    event.save({ notify_participants: true }).then(event => {
+    console.log(event);
+    });
+    res.render('CalendarInfoHome')
+ })
+
 
 module.exports = router1;
